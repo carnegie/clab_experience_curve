fixed lin. regr. & efficient weighting: gitignore

Method2.py
@@ -9,7 +9,7 @@ rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 df = pd.read_csv('ExpCurves.csv')
 
 method = 'regression'
-method = 'slope'
+# method = 'slope'
 
 # get slope for all technologies
 slopes = []
@@ -28,6 +28,8 @@ dferr = []
 dferr2 = []
 counterr = 0
 for tech in df['Tech'].unique():
+	# if 'Acry' not in tech:
+	# 	continue
 	# computing average technological slope based on all other technologies
 	slopeall = np.mean(slopes.loc[slopes['Tech'] != tech,'Slope'].values)
 	# computing technology specific slope
@@ -42,14 +44,14 @@ for tech in df['Tech'].unique():
 				(x[i] - x[N])
 			# add linear regression method
 			if method=='regression':
-				model = sm.OLS(y[:i+1], sm.add_constant(x[:i+1]))
+				model = sm.OLS(y[N:i+1], sm.add_constant(x[N:i+1]))
 				result = model.fit()
 				slope = result.params[1]
 			# compute error associated using slope M points after midpoint
 			for M in range(i+1, H):
 				pred =  y[i] + slope * (x[M] - x[i])
-				if method=='regression':
-					pred = result.params[0] + slope * x[M]
+				# if method=='regression':
+				# 	pred = result.params[0] + slope * x[M]
 				pred2 =  y[i] + slopeall * (x[M] - x[i])
 				error = (y[M] - (pred)) 
 				error2 = (y[M] - (pred2)) 
@@ -61,11 +63,16 @@ for tech in df['Tech'].unique():
 								error2, tech])
 				if np.abs(error2) < np.abs(error):
 					counterr += 1
-				# if 'Offshore_Gas_Pi' in tech and i==7 and M==8 and N ==0:
+				# if 'Acrylic' in tech and i==2 and N==1:
 				# 	print(tech, i, N, M)
-				# 	print(x[:i], y[:i], pred, pred2, y[M])
-				# 	print(x[i], x[N], y[i], y[N], slope, x[M], x[i])
-				# 	exit()
+				# 	print(x[:i+1], y[:i+1])
+				# 	print(x[i],x[N], y[i], y[N])
+				# 	print(x[i]-x[N], y[i]-y[N], slope)
+				# 	print(y[M], pred2, pred, x[i]-x[N], x[M]-x[i], slope)
+				# 	# print(x[:i], y[:i], pred, pred2, y[M])
+				# 	# print(x[i], x[N], y[i], y[N], slope, x[M], x[i])
+				# 	# exit()
+
 
 print('Percentage of cases where error', 
       ' is lower with average technological slope: ',
@@ -110,26 +117,25 @@ for i in range(1, len(frac)):
 					].copy()
 		# weight each technology equally in each bin
 		if select1.empty and select2.empty:
-			mean1[i-1, j-1] = 0.0
-			mean2[i-1, j-1] = 0.0
-			meandiff[i-1,j-1] = 0.0
-			fracavg[i-1,j-1] = 0.0
+			mean1[i-1, j-1] = np.nan
+			mean2[i-1, j-1] = np.nan
+			meandiff[i-1,j-1] = np.nan
+			fracavg[i-1,j-1] = np.nan
 			count[i-1,j-1] = 0.0
 		else:
 			mean1[i-1,j-1] = np.mean(select1['Error'].values**2)**0.5
 			mean2[i-1,j-1] = np.mean(select2['Error'].values**2)**0.5
-			# select1['Weights'] = [1/select1.loc[select1['Tech']==t,'Tech'].count() for t in select1['Tech'].values]
-			# select2['Weights'] = [1/select2.loc[select2['Tech']==t,'Tech'].count() for t in select2['Tech'].values]
-			# mean1[i-1, j-1] = np.average(select1['Error']**2, weights=select1['Weights'])**0.5
-			# mean2[i-1, j-1] = np.average(select2['Error']**2, weights=select2['Weights'])**0.5
-			mean1[i-1,j-1] = 0.0
-			mean2[i-1,j-1] = 0.0
-			ntechs = select1['Tech'].nunique()
-			for tech in select1['Tech'].unique():
-				sel1 = select1.loc[select1['Tech']==tech].copy()
-				sel2 = select2.loc[select2['Tech']==tech].copy()
-				mean1[i-1,j-1] += 1/(ntechs * sel1.count()[0]) * np.mean(sel1['Error'].values**2)**0.5
-				mean2[i-1,j-1] += 1/(ntechs * sel2.count()[0]) * np.mean(sel2['Error'].values**2)**0.5
+			# ## weighting each technology the same in each bin
+			# mean1[i-1,j-1] = 0.0
+			# mean2[i-1,j-1] = 0.0
+			# ntechs = select1['Tech'].nunique()
+			# for tech in select1['Tech'].unique():
+			# 	sel1 = select1.loc[select1['Tech']==tech].copy()
+			# 	sel2 = select2.loc[select2['Tech']==tech].copy()
+			# 	mean1[i-1,j-1] += 1/(ntechs * sel1.count()[0]) * np.sum(sel1['Error'].values**2)
+			# 	mean2[i-1,j-1] += 1/(ntechs * sel2.count()[0]) * np.sum(sel2['Error'].values**2)
+			# mean1[i-1,j-1] = mean1[i-1,j-1]**0.5
+			# mean2[i-1,j-1] = mean2[i-1,j-1]**0.5
 			meandiff[i-1,j-1] = mean2[i-1,j-1] - mean1[i-1,j-1]
 			fracavg[i-1,j-1] = np.sum(select2['Error'].values**2 < 
 									select1['Error'].values**2)/\
@@ -137,7 +143,6 @@ for i in range(1, len(frac)):
 			count[i-1,j-1] = (select1['Error'].count())
 			counttech[i-1,j-1] = (select1['Tech'].nunique())
 
-print(mean2)
 
 plt.figure()
 mean = mean1[::-1,:]
@@ -171,8 +176,6 @@ plt.suptitle('Average technology slope')
 
 plt.figure()
 mean = meandiff[::-1,:]
-mean[mean<0] = -1
-mean[mean>0] = 1
 divnorm = matplotlib.colors.TwoSlopeNorm(vcenter=0)
 im = plt.imshow(mean, aspect='auto', 
 		norm=divnorm, 
@@ -221,7 +224,9 @@ cbar.set_label('Bin size')
 plt.subplots_adjust(bottom=0.3, left=0.2, right=0.95, top=0.9)
 
 prob = 100*(perc > 50) * count/np.sum(count)
+print(sum(sum(prob)))
 prob2 = -100*(perc < 50) * count/np.sum(count)
+print(sum(sum(prob2)))
 prob[prob==0] = prob2[prob==0]
 
 plt.figure()